1473c97bfae6c7a23568a2ae038e0d0d9d06b996
PetterFogel <petterfogel96@gmail.com>
2023-07-18 18:47:48 +0200

    Implement new crisp styling

--- components/todos-form/TodosForm.tsx
+++ components/todos-form/TodosForm.tsx
@@ -28,17 +28,21 @@ export const TodosForm: FC<Props> = ({ onAddTodoClick }) => {
   return (
     <form
       onSubmit={submitHandler}
-      className="flex h-full flex-1 flex-col gap-3 rounded border bg-white p-3 ">
+      className="flex h-full flex-1 flex-col  gap-3 rounded-lg border bg-white  px-4 py-6 shadow-md  ring-1 ring-slate-900/5 dark:bg-slate-800">
       <input
-        className="rounded-lg border p-2"
+        className="rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
         placeholder="Title"
         type="text"
         value={value}
         onChange={inputChangeHandler}
       />
-      <input className="rounded-lg border p-2" placeholder="Description" type="text" />
+      <input
+        className="rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
+        placeholder="Description"
+        type="text"
+      />
       <button
-        className="text-l rounded-lg bg-sky-500 p-2 font-semibold text-white hover:bg-sky-700"
+        className="text-l rounded-lg bg-indigo-500 p-2 font-semibold text-white  ring-1 ring-gray-950/10 hover:bg-indigo-600"
         type="submit">
         Add
       </button>
--- components/todos-list/TodosList.tsx
+++ components/todos-list/TodosList.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Todo } from "@/types/todo";
 import { FC } from "react";
+import { Todo } from "@/types/todo";
 import { TrashIcon } from "@heroicons/react/24/solid";
 
 interface Props {
@@ -15,17 +15,19 @@ export const TodosList: FC<Props> = ({ todos, onDeleteTodoClick }) => {
   };
 
   return (
-    <ul role="list" className="h-full flex-1 flex-col divide-y rounded border bg-white p-3">
+    <ul
+      role="list"
+      className="h-full flex-1 flex-col divide-y rounded-lg border bg-white px-4 py-6 shadow-md ring-1 ring-slate-900/5 dark:bg-slate-800">
       {todos.map((todo) => (
         <li
           key={todo.id}
-          className="group/item flex items-center justify-between hover:bg-slate-100">
+          className="group/item flex items-center justify-between  hover:bg-slate-100">
           <div className="flex flex-col gap-1 p-2">
-            <p className="text font-semibold capitalize">{todo.title}</p>
+            <p className="text-base font-medium capitalize">{todo.title}</p>
           </div>
           <div
             onClick={() => deleteTodoHandler(todo.id)}
-            className="invisible m-2 cursor-pointer rounded-md p-1 text-gray-500 hover:bg-red-500 hover:text-yellow-50 group-hover/item:visible">
+            className="invisible m-2 cursor-pointer rounded-md p-1 text-gray-400 hover:text-red-500  group-hover/item:visible">
             <TrashIcon className="color h-5 w-5" />
           </div>
         </li>
