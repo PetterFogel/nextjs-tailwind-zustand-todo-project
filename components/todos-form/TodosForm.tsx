cbe0b9691452e1dc497e64d3775328ad93d67b46
PetterFogel <petterfogel96@gmail.com>
2023-07-18 18:54:28 +0200

    Fixed ring style bug

--- components/todos-form/TodosForm.tsx
+++ components/todos-form/TodosForm.tsx
@@ -30,19 +30,19 @@ export const TodosForm: FC<Props> = ({ onAddTodoClick }) => {
       onSubmit={submitHandler}
       className="flex h-full flex-1 flex-col  gap-3 rounded-lg border bg-white  px-4 py-6 shadow-md  ring-1 ring-slate-900/5 dark:bg-slate-800">
       <input
-        className="rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
+        className="appearance-none rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
         placeholder="Title"
         type="text"
         value={value}
         onChange={inputChangeHandler}
       />
       <input
-        className="rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
+        className="appearance-none rounded-md px-3 py-2 shadow ring-1 ring-gray-950/10"
         placeholder="Description"
         type="text"
       />
       <button
-        className="text-l rounded-lg bg-indigo-500 p-2 font-semibold text-white  ring-1 ring-gray-950/10 hover:bg-indigo-600"
+        className="text-l appearance-none rounded-lg bg-indigo-500 p-2 font-semibold text-white  ring-1 ring-gray-950/10 hover:bg-indigo-600"
         type="submit">
         Add
       </button>
