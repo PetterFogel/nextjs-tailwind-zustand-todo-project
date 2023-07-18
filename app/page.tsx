8a2dad4c3717f46dd44345ebe7c39c2ab6b9dd47
PetterFogel <petterfogel96@gmail.com>
2023-07-18 10:47:55 +0200

    Implement new styling

--- app/page.tsx
+++ app/page.tsx
@@ -8,46 +8,51 @@ export default function Home() {
     {
       id: "123",
       title: "Make lunch",
-      description: "Lunch for todays is meatballs and potatoes",
-      isDone: false,
+      description:
+        "Lunch for todays is meatballs and potatoes Lunch for todays is meatballs and potatoes",
+      isDone: false
     },
     {
       id: "456",
       title: "Do dishes",
       description: "Do the dishes after work",
-      isDone: false,
+      isDone: false
     },
+    {
+      id: "567",
+      title: "Do dishes",
+      description: "Do the dishes after work",
+      isDone: false
+    }
   ]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between sm:p-24 p-4">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm">
-        <h1 className="text-4xl p-4 text-center">Todays Todos</h1>
-        <div className=" p-4 rounded-lg">
-          <form className="grid grid-cols-6 items-center text-black">
-            <input
-              className="col-span-3 p-3 border"
-              placeholder="Enter Item"
-              type="text"
-            />
-            <input
-              className="col-span-2 p-3 border mx-3"
-              placeholder="Enter $"
-              type="number"
-            />
+    <main className="m-auto max-w-5xl p-4">
+      <div className="flex flex-col">
+        <h1 className="p-4 text-center text-4xl">Todays Todos</h1>
+        <div className="selection:mono flex flex-col gap-4 text-sm lg:flex-row">
+          <form className="flex h-full flex-1 flex-col gap-3 rounded border bg-white p-3 ">
+            <input className="rounded-lg border p-2" placeholder="Title" type="text" />
+            <input className="rounded-lg border p-2" placeholder="Description" type="text" />
             <button
-              className="text-black border border-black hover:bg-slate-300 p-3 text-l"
-              type="submit"
-            >
+              className="text-l rounded-lg bg-violet-500 p-2 text-white hover:bg-violet-600"
+              type="submit">
               Add
             </button>
           </form>
-          <ul className="p-6 divide-y divide-slate-200 bg-white">
+          <ul
+            role="list"
+            className="flex flex-1 flex-col  divide-slate-200 rounded border bg-white p-3">
             {todos.map((todo, i) => (
-              <li key={i} className="flex py-4 first:pt-0 last:pb-0">
-                <div className="p-4 w-full flex flex-col gap-3">
-                  <span className="capitalize">{todo.title}</span>
-                  <span>{todo.description}</span>
+              <li key={i} className="group/item flex items-center rounded hover:bg-slate-100">
+                <div className="flex flex-col gap-1 p-2">
+                  <p className="text font-semibold capitalize">{todo.title}</p>
+                  <p className="break-normal text-xs capitalize text-slate-500">
+                    {todo.description}
+                  </p>
+                </div>
+                <div className="invisible m-2 cursor-pointer rounded-md px-2 py-1 hover:bg-red-500 hover:text-yellow-50 group-hover/item:visible">
+                  <p>Delete</p>
                 </div>
               </li>
             ))}
