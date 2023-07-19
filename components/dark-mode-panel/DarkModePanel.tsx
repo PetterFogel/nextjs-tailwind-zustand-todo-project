cc065d8dc0468a9c14293d5f13b3b5cc4d92842b
PetterFogel <petterfogel96@gmail.com>
2023-07-19 15:43:04 +0200

    Implement dark mode panel

---
+++ components/dark-mode-panel/DarkModePanel.tsx
@@ -0,0 +1,41 @@
+import { FC, useEffect, useState } from "react";
+import { Switch } from "@headlessui/react";
+
+export const DarkModePanel: FC = () => {
+  const [enabled, setEnabled] = useState(false);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setEnabled(true);
+      document.documentElement.classList.add("dark");
+      return;
+    }
+    setEnabled(false);
+    document.documentElement.classList.remove("dark");
+  }, [enabled]);
+
+  const toggleSwitchHandler = (checked: boolean) => {
+    setEnabled(checked);
+    if (checked) return (localStorage.theme = "dark");
+    localStorage.theme = "light";
+  };
+  return (
+    <Switch
+      checked={enabled}
+      onChange={toggleSwitchHandler}
+      className={`${
+        enabled ? "bg-indigo-500" : "bg-gray-200"
+      } relative inline-flex h-6 w-11 items-center rounded-full`}>
+      <span className="sr-only">Enable notifications</span>
+      <span
+        className={`${
+          enabled ? "translate-x-6" : "translate-x-1"
+        } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+      />
+    </Switch>
+  );
+};
--- components/todos-container/TodosContainer.tsx
+++ components/todos-container/TodosContainer.tsx
@@ -1,13 +1,12 @@
 "use client";
 import { Todo } from "@/types/todo";
 import { TodosForm } from "../todos-form/TodosForm";
-import { Switch } from "@headlessui/react";
 import { TodosList } from "../todos-list/TodosList";
 import { FC, useEffect, useState } from "react";
+import { DarkModePanel } from "../dark-mode-panel/DarkModePanel";
 
 export const TodosContainer: FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [enabled, setEnabled] = useState(false);
 
   const addTodoHandler = (newTodo: Todo) => setTodos((prevState) => [...prevState, newTodo]);
 
@@ -23,42 +22,9 @@ export const TodosContainer: FC = () => {
     localStorage.setItem("todoList", JSON.stringify(todos));
   }, [todos]);
 
-  useEffect(() => {
-    if (
-      typeof window !== "undefined"
-        ? localStorage.theme === "dark" ||
-          (!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
-        : false
-    ) {
-      setEnabled(true);
-      document.documentElement.classList.add("dark");
-    } else {
-      setEnabled(false);
-      document.documentElement.classList.remove("dark");
-    }
-  }, [enabled]);
-
-  const toggleSwitchHandler = (checked: boolean) => {
-    setEnabled(checked);
-    if (checked) return (localStorage.theme = "dark");
-    localStorage.theme = "light";
-  };
-
   return (
     <div className="flex flex-col bg-white dark:bg-black">
-      <Switch
-        checked={enabled}
-        onChange={toggleSwitchHandler}
-        className={`${
-          enabled ? "bg-indigo-500" : "bg-gray-200"
-        } relative inline-flex h-6 w-11 items-center rounded-full`}>
-        <span className="sr-only">Enable notifications</span>
-        <span
-          className={`${
-            enabled ? "translate-x-6" : "translate-x-1"
-          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-        />
-      </Switch>
+      <DarkModePanel />
       <h1 className="p-4 text-center text-4xl">Todays Todos</h1>
       <div className="flex flex-col gap-4 text-sm lg:flex-row">
         <TodosForm onAddTodoClick={addTodoHandler} />
