bd347d4e3695a33d910b0a72e3a9b69f74655a8d
PetterFogel <petterfogel96@gmail.com>
2023-07-20 13:00:50 +0200

    Implement logic to display description

--- components/todo-item/TodoItem.tsx
+++ components/todo-item/TodoItem.tsx
@@ -14,21 +14,26 @@ export const TodoItem: FC<Props> = ({ todo, onDeleteTodoClick, onCheckTodoClick 
   };
 
   return (
-    <li className="group/item flex items-center justify-between rounded-md p-3 hover:bg-slate-100 dark:hover:bg-slate-900">
-      <div className="flex items-center justify-center gap-3">
-        <input
-          onChange={checkTodoHandler}
-          checked={todo.isDone}
-          id="checked-checkbox"
-          type="checkbox"
-        />
-        <p className="text-base font-medium capitalize">{todo.title}</p>
+    <li className="group/item flex flex-col gap-3 rounded-md p-3 hover:bg-slate-100 dark:hover:bg-slate-900">
+      <div className="flex">
+        <div className="flex w-full gap-3">
+          <input
+            onChange={checkTodoHandler}
+            checked={todo.isDone}
+            id="checked-checkbox"
+            type="checkbox"
+          />
+          <p className="text-base font-medium capitalize">{todo.title}</p>
+        </div>
+        <div
+          onClick={() => onDeleteTodoClick(todo.id)}
+          className="invisible cursor-pointer rounded-md text-gray-400 hover:text-red-500  group-hover/item:visible">
+          <TrashIcon className="color h-5 w-5" />
+        </div>
       </div>
-      <div
-        onClick={() => onDeleteTodoClick(todo.id)}
-        className="invisible cursor-pointer rounded-md text-gray-400 hover:text-red-500  group-hover/item:visible">
-        <TrashIcon className="color h-5 w-5" />
-      </div>
+      <p className="hidden text-sm capitalize text-slate-500 group-hover/item:block dark:text-slate-400">
+        {todo.description}
+      </p>
     </li>
   );
 };
