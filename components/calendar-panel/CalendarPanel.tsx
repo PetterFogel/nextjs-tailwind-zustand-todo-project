eba35cf0360875f7f709e310b18eb730a57130d0
PetterFogel <petterfogel96@gmail.com>
2023-07-20 14:43:16 +0200

    Implement logic to change date

--- components/calendar-panel/CalendarPanel.tsx
+++ components/calendar-panel/CalendarPanel.tsx
@@ -3,26 +3,36 @@ import { FC, useEffect, useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 
 export const CalendarPanel: FC = () => {
+  const [dateValue, setDateValue] = useState(0);
   const [currentDate, setCurrentDate] = useState({
     dayName: "",
     date: ""
   });
+
   useEffect(() => {
-    const currentDate = new Date();
+    const newDate = new Date();
+    newDate.setDate(newDate.getDate() + dateValue);
+
     setCurrentDate({
-      dayName: currentDate.toLocaleString("en-GB", { weekday: "long" }),
-      date: currentDate.toLocaleString("en-GB", { dateStyle: "medium" })
+      dayName: newDate.toLocaleString("en-GB", { weekday: "long" }),
+      date: newDate.toLocaleString("en-GB", { dateStyle: "medium" })
     });
-  }, []);
+  }, [dateValue]);
 
   return (
     <div className="flex items-center justify-center gap-6">
-      <ChevronLeftIcon className="color h-10 w-10 cursor-pointer text-slate-500" />
-      <div className="flex flex-col gap-2">
+      <ChevronLeftIcon
+        className="color h-10 w-10 cursor-pointer text-slate-500"
+        onClick={() => setDateValue((prevState) => prevState - 1)}
+      />
+      <div className="flex w-56 flex-col gap-2">
         <h1 className="text-center text-4xl">{currentDate.dayName}</h1>
         <h1 className="text-center text-xl text-slate-500">{currentDate.date}</h1>
       </div>
-      <ChevronRightIcon className="color h-10 w-10 cursor-pointer text-slate-500" />
+      <ChevronRightIcon
+        className="color h-10 w-10 cursor-pointer text-slate-500"
+        onClick={() => setDateValue((prevState) => prevState + 1)}
+      />
     </div>
   );
 };
